7bbd2e909b88b573d7dc8262071f0d95e8e46f6e
Sabik-Kavungal <sabikkavungal123@gmail.com>
2024-11-21 12:27:42 +0530

    ddd

--- utils/cors.go
+++ utils/cors.go
@@ -8,7 +8,7 @@ import (
 // SetupCORS sets up Cross-Origin Resource Sharing (CORS) middleware for the application
 func SetupCORS() gin.HandlerFunc {
 	return cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000", "https://your-production-domain.com"}, // Replace with allowed origins
+		AllowOrigins:     []string{"http://localhost:3000", "https://quickz.onrender.com"}, // Replace with allowed origins
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
