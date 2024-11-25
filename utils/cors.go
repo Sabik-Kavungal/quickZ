84a223dff95a4765a064273bda215167e39922ec
Sabik-Kavungal <sabikkavungal123@gmail.com>
2024-11-25 19:48:06 +0530

    cors isseus fixed

--- utils/cors.go
+++ utils/cors.go
@@ -1,28 +1,27 @@
 package utils
 
 import (
+	
 	"net/http"
-	"log"
 )
 
-// EnableCORS sets up CORS middleware for both local development and production use.
 func EnableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		origin := r.Header.Get("Origin")
-		log.Printf("CORS Request from Origin: %s", origin)
+		if origin == "" {
+			origin = "*" // Allow any origin if not specified
+		}
 
 		w.Header().Set("Access-Control-Allow-Origin", origin)
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
-		// Handle preflight (OPTIONS) requests
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusOK)
 			return
 		}
 
-		// Pass the request to the next middleware/handler
 		next.ServeHTTP(w, r)
 	})
 }
