60b7ee9e757e68e4885275c597e5decc7c934ef4
Sabik-Kavungal <sabikkavungal123@gmail.com>
2024-11-25 19:52:56 +0530

    cors isseus fixed

--- utils/cors.go
+++ utils/cors.go
@@ -1,27 +1,19 @@
 package utils
 
 import (
-	
 	"net/http"
 )
 
 func EnableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		origin := r.Header.Get("Origin")
-		if origin == "" {
-			origin = "*" // Allow any origin if not specified
-		}
-
-		w.Header().Set("Access-Control-Allow-Origin", origin)
+		w.Header().Set("Access-Control-Allow-Origin", "*") // Allow all origins for testing
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
-
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusOK)
 			return
 		}
-
 		next.ServeHTTP(w, r)
 	})
 }
